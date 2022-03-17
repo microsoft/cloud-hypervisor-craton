d4a7596709eb00e141061c815b8927379d0ba3d7
Muminul Islam <muislam@microsoft.com>
2022-03-17 12:10:35 -0700

    Dead code and public modifier
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- arch/src/aarch64/fdt_edit.rs
+++ arch/src/aarch64/fdt_edit.rs
@@ -6,7 +6,8 @@ use vm_fdt::{FdtWriter, FdtWriterResult, Error};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-struct NodeBuilder {
+#[allow(dead_code)]
+pub struct NodeBuilder {
     pub name: String,
 
     /// A list of node properties, `(key, value)`.
@@ -15,6 +16,7 @@ struct NodeBuilder {
 
 
 impl NodeBuilder {
+    #[allow(dead_code)]
     fn new(node_name: &str) -> NodeBuilder {
         let mut _props = Vec::new();
         NodeBuilder {
@@ -22,21 +24,23 @@ impl NodeBuilder {
             props: _props,
         }
     }
+    #[allow(dead_code)]
     fn add_property(&mut self, prop_name: &str, value:  &[u8]) -> Result<()> {
         self.props.push((prop_name.to_string(), value.to_owned()));
         Ok(())
     }
-      /// Write an empty property.
-      pub fn property_null(&mut self, name: &str) -> Result<()> {
+    #[allow(dead_code)]
+    /// Write an empty property.
+    pub fn property_null(&mut self, name: &str) -> Result<()> {
         self.add_property(name, &[])
     }
-
+    #[allow(dead_code)]
     /// Write a string property.
     pub fn property_string(&mut self, name: &str, val: &str) -> Result<()> {
         let cstr_value = CString::new(val).map_err(|_| Error::InvalidString)?;
         self.add_property(name, cstr_value.to_bytes_with_nul())
     }
-
+    #[allow(dead_code)]
     /// Write a stringlist property.
     pub fn property_string_list(&mut self, name: &str, values: Vec<String>) -> Result<()> {
         let mut bytes = Vec::new();
@@ -46,17 +50,17 @@ impl NodeBuilder {
         }
         self.add_property(name, &bytes)
     }
-
+    #[allow(dead_code)]
     /// Write a 32-bit unsigned integer property.
     pub fn property_u32(&mut self, name: &str, val: u32) -> Result<()> {
         self.add_property(name, &val.to_be_bytes())
     }
-
+    #[allow(dead_code)]
     /// Write a 64-bit unsigned integer property.
     pub fn property_u64(&mut self, name: &str, val: u64) -> Result<()> {
         self.add_property(name, &val.to_be_bytes())
     }
-
+    #[allow(dead_code)]
     /// Write a property containing an array of 32-bit unsigned integers.
     pub fn property_array_u32(&mut self, name: &str, cells: &[u32]) -> Result<()> {
         let mut arr = Vec::with_capacity(size_of_val(cells));
@@ -65,7 +69,7 @@ impl NodeBuilder {
         }
         self.add_property(name, &arr)
     }
-
+    #[allow(dead_code)]
     /// Write a property containing an array of 64-bit unsigned integers.
     pub fn property_array_u64(&mut self, name: &str, cells: &[u64]) -> Result<()> {
         let mut arr = Vec::with_capacity(size_of_val(cells));
@@ -74,6 +78,7 @@ impl NodeBuilder {
         }
         self.add_property(name, &arr)
     }
+    #[allow(dead_code)]
     pub fn build(&self) -> device_tree::Node {
 
         device_tree::Node {
@@ -83,7 +88,8 @@ impl NodeBuilder {
         }
     }
 }
-fn copy_from_FdtTree(dt: &DeviceTree) -> FdtWriterResult<Vec<u8>> {
+#[allow(dead_code)]
+pub fn copy_from_fdt_tree(dt: &DeviceTree) -> FdtWriterResult<Vec<u8>> {
 
     let mut writer = FdtWriter::new().unwrap();
     writer.set_boot_cpuid_phys(dt.boot_cpuid_phys);
@@ -95,11 +101,12 @@ fn copy_from_FdtTree(dt: &DeviceTree) -> FdtWriterResult<Vec<u8>> {
         writer.property(&name, &value)?;
     }
     for child in dt_root.children.iter() {
-        add_node_writer(&mut writer, &child);
+        add_node_writer(&mut writer, &child)?;
     }
     writer.end_node(root)?;
     return writer.finish()
 }
+#[allow(dead_code)]
 fn add_node_writer(writer: &mut FdtWriter, node: &device_tree::Node) -> FdtWriterResult<()> {
     let child = writer.begin_node(&node.name)?;
     for prop in node.props.iter() {
@@ -113,6 +120,7 @@ fn add_node_writer(writer: &mut FdtWriter, node: &device_tree::Node) -> FdtWrite
     writer.end_node(child)?;
     Ok(())
 }
+#[allow(dead_code)]
 fn find_parent_node<'a>(root: &'a mut device_tree::Node, name: &str) -> Option<&'a mut device_tree::Node>{
 
     if root.name == name {
@@ -131,13 +139,15 @@ fn find_parent_node<'a>(root: &'a mut device_tree::Node, name: &str) -> Option<&
     }
     return None
 }
-fn find_parent<'a>(dt: &'a mut DeviceTree, name: &str) -> Option<&'a mut device_tree::Node>{
+#[allow(dead_code)]
+pub fn find_parent<'a>(dt: &'a mut DeviceTree, name: &str) -> Option<&'a mut device_tree::Node>{
     return find_parent_node(&mut dt.root, name);
 }
+#[allow(dead_code)]
 fn add_child(root: &mut device_tree::Node, child: device_tree::Node) {
     root.children.push(child);
 }
-
+#[allow(dead_code)]
 fn create_vector_for_reg(acells: u32, adress: u64, scells: u32, size:u64) -> FdtWriterResult<Vec<u32> > {
     let ys: [u64; 4] = [acells.into(), adress, scells.into(), size];
     let mut propcells: Vec<u32> = Vec::with_capacity(ys.len()); 
@@ -164,7 +174,8 @@ fn create_vector_for_reg(acells: u32, adress: u64, scells: u32, size:u64) -> Fdt
     }
     return Ok(propcells)
 }
-fn edit_FdtTree_with_writer(dt: &DeviceTree,node_name: &str, prop_name: &str, value: &Vec<u8> ) -> FdtWriterResult<Vec<u8>> {
+#[allow(dead_code)]
+pub fn edit_fdt_tree_with_writer(dt: &DeviceTree,node_name: &str, prop_name: &str, value: &Vec<u8> ) -> FdtWriterResult<Vec<u8>> {
 
     let mut writer = FdtWriter::new().unwrap();
     writer.set_boot_cpuid_phys(dt.boot_cpuid_phys);
@@ -181,7 +192,8 @@ fn edit_FdtTree_with_writer(dt: &DeviceTree,node_name: &str, prop_name: &str, va
     writer.end_node(root)?;
     return writer.finish()
 }
-fn edit_node_with_writer(writer: &mut FdtWriter, node: &device_tree::Node, node_name: &str, prop_name: &str, new_value: &Vec<u8> ) -> FdtWriterResult<()> {
+#[allow(dead_code)]
+pub fn edit_node_with_writer(writer: &mut FdtWriter, node: &device_tree::Node, node_name: &str, prop_name: &str, new_value: &Vec<u8> ) -> FdtWriterResult<()> {
     let child = writer.begin_node(&node.name)?;
     for prop in node.props.iter() {
 
@@ -200,13 +212,14 @@ fn edit_node_with_writer(writer: &mut FdtWriter, node: &device_tree::Node, node_
     writer.end_node(child)?;
     Ok(())
 }
-fn modify_prop_regs(dt: &DeviceTree, node_name: &str, prop_name: &str, acells: u32, adress: u64, scells: u32, size:u64) -> FdtWriterResult<Vec<u8>>  {
+#[allow(dead_code)]
+pub fn modify_prop_regs(dt: &DeviceTree, node_name: &str, prop_name: &str, acells: u32, adress: u64, scells: u32, size:u64) -> FdtWriterResult<Vec<u8>>  {
     let new_val = create_vector_for_reg(acells,adress,scells, size)?;
     let mut arr = Vec::with_capacity(size_of_val(&new_val));
     for c in new_val.iter() {
         arr.extend(&c.to_be_bytes());
     }
-    edit_FdtTree_with_writer(dt,node_name,prop_name, &arr)
+    edit_fdt_tree_with_writer(dt,node_name,prop_name, &arr)
 }
 
 fn edit_node_int(node: &mut device_tree::Node, prop_name: &str, new_value: &Vec<u32>) -> FdtWriterResult<()> {
@@ -221,7 +234,8 @@ fn edit_node_int(node: &mut device_tree::Node, prop_name: &str, new_value: &Vec<
     }
     Ok(())
 }
-fn edit_FdtTree(dt: &mut DeviceTree,node_name: &str, prop_name: &str, value: &Vec<u32> ) -> FdtWriterResult<()> {
+#[allow(dead_code)]
+pub fn edit_fdt_tree(dt: &mut DeviceTree,node_name: &str, prop_name: &str, value: &Vec<u32> ) -> FdtWriterResult<()> {
     let opt_node = find_node(dt, node_name);
     if let Some(node) = opt_node {
         edit_node_int(node, prop_name, value)?
@@ -240,7 +254,8 @@ fn find_node_util<'a>(root: &'a mut device_tree::Node, name: &str) -> Option<&'a
 
     return None
 }
-fn find_node<'a>(dt: &'a mut DeviceTree, name: &str) -> Option<&'a mut device_tree::Node>{
+#[allow(dead_code)]
+pub fn find_node<'a>(dt: &'a mut DeviceTree, name: &str) -> Option<&'a mut device_tree::Node>{
 
     return find_node_util(&mut dt.root, name);
 }
