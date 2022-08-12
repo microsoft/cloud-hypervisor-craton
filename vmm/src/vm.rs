75063702fccfe5323c9e032bb6b703e2734a7a37
Nuno Das Neves <nudasnev@microsoft.com>
2022-08-12 17:25:37 +0000

    Fix for virtio-mmio irq number
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -3491,7 +3491,7 @@ mod tests {
                 },
             ),
             (
-                (DeviceType::Virtio(1), "virtio".to_string()),
+                (DeviceType::VirtioMmio(1), "virtio".to_string()),
                 MmioDeviceInfo {
                     addr: LEN,
                     len: LEN,
